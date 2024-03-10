5b3c2e56b830525f78a60a2be3efcf4672f462b6
21120447 <haonhat2729@gmail.com>
2024-03-10 14:09:38 +0700

    add footer view

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,8 +1,97 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <footer className='py-4'>
+        <div className='container-xxl'>
+          <div className='row align-items-center'>
+            <div className='col-5'>
+              <div className='footer-top-data d-flex gap-30 align-items-center'>
+                <img src='images/newsletter.png' alt='newsletter' />
+                <h2 className='mb-0 text-white'>Sign Up for Newsletter</h2>
+              </div>
+            </div>
+            <div className='col-7'>
+              <div className="input-group">
+                <input type="text" className="form-control py-1" placeholder="Your email address" aria-label="Your email address" aria-describedby="basic-addon2" />
+                <span className="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className='py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-4'>
+              <h4 className='text-white'>Contact US</h4>
+              <div>
+                <address className='text-white fs-6'>Hno : 277 Near Vill chopal, <br /> Sonipat, Haryana <br />
+                  PinCode: 131103
+                </address>
+                <a href='tel:+91 8264954234' className='mt-3 d-block mb-1 text-white'>+91 8264954234</a>
+                <a href='mailto:admin@gmail.com' className='mt-2 d-block mb-0 text-white'>admin@gmail.com</a>
+                <div className='social_icons d-flex align-items-center gap-30 mt-4'>
+                  <a href='' className='text-white'>
+                    <BsLinkedin className='fs-4' />
+                  </a>
+                  <a href='' className='text-white'>
+                    <BsInstagram className='fs-4' />
+                  </a>
+                  <a href='' className='text-white'>
+                    <BsGithub className='fs-4' />
+                  </a>
+                  <a href='' className='text-white'>
+                    <BsYoutube className='fs-4' />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className='col-3'>
+              <h4 className='text-white'>Information</h4>
+              <div className='footer-links d-flex flex-column'>
+                <Link className="text-white py-2 mb-1">Privacy Policy</Link>
+                <Link className="text-white py-2 mb-1">Refund Policy</Link>
+                <Link className="text-white py-2 mb-1">Shipping Policy</Link>
+                <Link className="text-white py-2 mb-1">Terms & Conditions</Link>
+                <Link className="text-white py-2 mb-1">Blogs</Link>
+              </div>
+            </div>
+            <div className='col-3'>
+              <h4 className='text-white'>Account</h4>
+              <div className='footer-links d-flex flex-column'>
+                <Link className="text-white py-2 mb-1">About Us</Link>
+                <Link className="text-white py-2 mb-1">Faq</Link>
+                <Link className="text-white py-2 mb-1">Contact</Link>
+              </div>
+            </div>
+            <div className='col-2'>
+              <h4 className='text-white'>Quick Links</h4>
+              <div className='footer-links d-flex flex-column'>
+                <Link className="text-white py-2 mb-1">Laptops</Link>
+                <Link className="text-white py-2 mb-1">Headphones</Link>
+                <Link className="text-white py-2 mb-1">Tablets</Link>
+                <Link className="text-white py-2 mb-1">Watch</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className='py-4'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <p className='text-center mb-0 text-white'>&copy; {new Date().getFullYear()}; Powered by Developer's Corner</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   )
 }
 
