4dfbfd4b9d047eed7c64d9bcd0b3bb46a0698d3f
21120447 <haonhat2729@gmail.com>
2024-03-10 23:48:20 +0700

    add st

--- frontend/src/pages/CompareProduct.js
+++ frontend/src/pages/CompareProduct.js
@@ -1,8 +1,58 @@
 import React from 'react'
+import BreadCrumb from '../components/BreadCrumb'
+import Meta from '../components/Meta'
 
 const CompareProduct = () => {
   return (
-    <div>CompareProduct</div>
+    <>
+      <Meta title='Compare Products' />
+      <BreadCrumb title='Compare Products' />
+      <div className='compare-product-wrapper py-5 home-wrapper-2'>
+        <div className='container-xl'>
+          <div className='row'>
+            <div className='col-3'>
+              <div className='compare-product-card position-relative'>
+                <img src='images/cross.svg' alt='cross' className='position-absolute cross img-fluid' />
+                <div className='product-card-image'>
+                  <img src='images/watch.jpg' alt='watch' className='img-fluid' />
+                </div>
+                <div className='compare-product-details'>
+                  <h5 className='title'>
+                    Honor T1 7.0 1 GB RAM 8 GB ROM 7 Inch With Wi-Fi+3G Tablet
+                  </h5>
+                  <h6 className='price mb-3'>$100</h6>
+                  <div>
+                    <div className='product-detail'>
+                      <h5>Brand:</h5>
+                      <p>Havels</p>
+                    </div>
+                    <div className='product-detail'>
+                      <h5>Type:</h5>
+                      <p>Havels</p>
+                    </div>
+                    <div className='product-detail'>
+                      <h5>Availability:</h5>
+                      <p>Havels</p>
+                    </div>
+                    <div className='product-detail'>
+                      <h5>Color:</h5>
+                      <p>Havels</p>
+                    </div>
+                    <div className='product-detail'>
+                      <h5>Size:</h5>
+                      <div className='d-flex gap-10'>
+                        <p>S</p>
+                        <p>M</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
