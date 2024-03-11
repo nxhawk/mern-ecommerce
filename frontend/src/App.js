2bbbc30661b7a253a91302f4386d2fde5f288fea
21120447 <haonhat2729@gmail.com>
2024-03-11 09:32:03 +0700

    forgot password page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,8 @@ import OurStore from './pages/OurStore';
 import Blog from './pages/Blog';
 import CompareProduct from './pages/CompareProduct';
 import Wishlist from './pages/Wishlist';
+import Login from './pages/Login';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
@@ -23,6 +25,8 @@ function App() {
             <Route path="blogs" element={<Blog />} />
             <Route path="compare-product" element={<CompareProduct />} />
             <Route path="wishlist" element={<Wishlist />} />
+            <Route path="login" element={<Login />} />
+            <Route path="forgot-password" element={<ForgotPassword />} />
           </Route>
         </Routes>
       </BrowserRouter>
